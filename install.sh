8f3acfdb6b147da75121bcc18e037ebcb70401c0
Patrick Hart <thrifus@gmail.com>
2015-12-29 11:21:57 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 sudo curl -# https://raw.githubusercontent.com/thrifus/f04/master/f04 -o /usr/bin/f04
 sudo chmod +x /usr/bin/f04
 
-printf "\n\033[1mCongrats! f04 is setup and ready to use and confuse!\n"
+printf "\n\033[1mCongrats! f04 is setup and ready to use and confuse!\033[0m\n"
 
 f04
